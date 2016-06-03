a17d146bb1d0bfb10f43f3db3991dea7da2c4502
Prerna Gupta <pennygupta@hotmail.com>
2016-06-03 17:01:45 +0800

    main page done .
    h1 not working and need to get rid of surrounding blue box

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 $(document).ready (function(){
 
+  $("#collapse-menu").click(function(event){
+    event.preventDefault();
+  })
 
+ //trigger the 3 bars to toggle classes
+  $(".icon").click(function(){
+    $("#nav").fadeToggle(400);
+    $("#top-line").toggleClass("top-transform");
+    $("#middle-line").toggleClass("mid-transform");
+    $("#bottom-line").toggleClass("bottom-transform");
+  });
 
-
-
+//   //to minimise overlay with esc
+  $(document).keydown(function(event) {
+  if (event.keyCode == 27) {
+    $("#nav").fadeOut(500);
+    $("#top-line").removeClass("top-transform");
+    $("#middle-line").removeClass("mid-transform");
+    $("#bottom-line").removeClass("bottom-transform");
+  }
 });
+
+
+})//closing bracket
